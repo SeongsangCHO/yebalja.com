373dee4dec2f83b0b141ff011a31d5aae5c5801f
seongsangCHO <secho@student.42seoul.kr>
2020-08-07 19:57:51 +0900

    Add comment json.js

--- back/routes/json.js
+++ back/routes/json.js
@@ -59,68 +59,69 @@ router.get('/reviews', function(req, res, next){
   })
 })
 
-const reduceDouble = function(arr){
-  return categoryArray = arr.filter((item, index) => arr.indexOf(item) === index);
+const hasKey = (arr, key, value) => {
+  let len = arr.length;
+  if (!arr[len - 1] || arr[len - 1][key] != value){
+    return false;
+  } else {
+    return true;
+  }
 }
 
 router.get('/qnas', function(req, res, next){
   let sql;
-  let getCategory = 'select distinct title as category from categories';
   sql = "select replace(programs.link, '/', '') as program,  categories.title as category,  categories.eventkey as eventKey,  subcategories.title as title,  subcategories.link as href, qnas.q , qnas.a from programs  inner join categories on programs.id = categories.programs_id inner join subcategories on categories.id = subcategories.categories_id  inner join qnas on qnas.subcategories_id = subcategories.id";
-  // let category = [];
-  // db.query(getCategory, (error, result) => {
-  //   if(error) throw error;
-  //   console.log(result);
 
-  // })
   db.query(sql, (error, result) => {
     if(error) throw error;
     let obj = {};
-    let categoryTemp = [];
-    let eventkeyTemp = [];
+    let arr;
     result.map(v => {
       if (!(v.program in obj))
       {
         obj[v.program] = [];
       }
-    categoryTemp.push(v.category);
-    eventkeyTemp.push(v.eventKey);
-    //  obj[v.program].push(v);
+      //각 프로그램마다 배열을 생성 => 객체의 프로그램 키에 해당하는 값이 arr배열임.
+      arr = obj[v.program];
+      // arr에 마지막 객체에 해당하는 category키의 값이 v.category이 아닐때,
+      // result의 모든 객체에는 중복되는 카테고리가 많다.
+      // arr배열에는 처음에 아무것도 없으므로, 지원/선발이라는 카테고리와 이후 2가지에 해당하는 eventKey를 할당한다.
+      // 그렇게해서 arr배열에는 [ {category : '1번값,..} {2번값}, {3번값}] 이 들어가게 된다.
+      // category의 값들을 배열에 push하고, filter메소드를 통해 중복값 제거하는 방법도 있음.
+      if (!hasKey(arr, 'category', v.category)){
+        let obj2 = {
+          'category' : v.category,
+          'eventKey' : v.eventKey,
+          'subCategory' : []
+        }
+        arr.push(obj2);
+      }
+      // length - 1 은 현재 프로그램이라는 뜻. map반복문이 프로그램을 쌓아가기 때문에 맨 끝의 프로그램만 처리해야함
+      let arr2 = arr[arr.length - 1].subCategory;//현재프로그램에서 현재 카테고리의 서브카테고리에 해당하는 배열을 할당
+      // 반복문의 끝에 해당하는 프로그램에 해당하는 subCategory키의 주소를 <매핑> (?맞나요?)
+      if (!hasKey(arr2, 'title', v.title)){
+        let obj3 = {
+          'title' : v.title,
+          'href' : v.href,
+          'qna' : []
+        }
+        arr2.push(obj3);
+      }
+
+      let arr3 = arr2[arr2.length - 1].qna;
+      if (!hasKey(arr3, 'q', v.q)){
+        let obj4 = {
+          'q' : v.q,
+          'a' : v.a
+        }
+        arr3.push(obj4);
+      }
+    })
     console.log(obj);
-     delete v['program'];
-    })
-    //카테고리의 중복요소 제거
-    categoryArray = reduceDouble(categoryTemp);
-    //let categoryArray = categoryTemp.filter((item, index) => categoryTemp.indexOf(item) === index);
-    let eventkeyArray = eventkeyTemp.filter((item, index) => eventkeyTemp.indexOf(item) === index);
-
-    console.log(categoryArray);
-    let programTitle = Object.keys(obj);
-    programTitle.forEach(function(o){
-      categoryArray.forEach(function(key, idx){
-        obj[o].push({category : key,
-        eventKey : eventkeyArray[idx],
-        subCategory : []
-       }
-      );
-       // obj[o].category = key;
-      });
-      // eventkeyArray.forEach(function(key){
-      //   obj[o].push({evnetKey : key})
-      // })
-      //obj[o]에 각각 3개의 키를 갖는 3개의 객체를 쌓아야함.
-      //category접근은 result[0].category
-      //그럼 모든 result배열의 원소에서 중복없는 category값을 뽑아서 배열에 넣어보자
-      console.log(obj[o]);
-
-    })
-    // let programTitle = Object.keys(obj);
-    // programTitle.forEach(function(o){
-    //   console.log(obj[o]);
-    // })
     res.json(obj);
   })
 })
+
 router.get('/table', (req, res) => {
   db.query(`
     select json_object(
